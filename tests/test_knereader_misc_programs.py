5bc03e2efd60ce5eb53c2af7b90d1393c2109537
Felix Schwarz <felix.schwarz@schwarz.eu>
2008-07-03 20:49:02 +0200

    added all other known test data

--- tests/test_knereader_misc_programs.py
+++ tests/test_knereader_misc_programs.py
@@ -20,6 +20,22 @@ class TestKneReaderMonkeyKassenbuch(SampleDataReaderCase):
 
 
 
+class TestKneReaderMonkeyKassenbuchAccountLabels(SampleDataReaderCase):
+    def setUp(self):
+        datafile = 'monkey_kassenbuch_account_labels'
+        super(TestKneReaderMonkeyKassenbuchAccountLabels, self).setUp(datafile)
+    
+    def test_account_labels(self):
+        self.assertEqual(1, self.reader.get_number_of_files())
+        dfile = self.reader.get_file(0)
+        data_lines = dfile.get_master_data_lines()
+        self.assertEqual(286, len(data_lines))
+        line2 = data_lines[1]
+        self.assertEqual(65, line2.key)
+        self.assertEqual(u"Unbebaute Grundstücke", line2.text)
+
+
+
 # TZ EasyBuch has many format errors, they are not reliable and there much too
 # many errors to fix them myself.
 #class TestKneReaderTZEasyBuch(SampleDataReaderCase):
@@ -43,6 +59,22 @@ class TestKneReaderMMSKassenbuch(SampleDataReaderCase):
         self.assertEqual('SELF ID: 15540', tfile.get_metadata()['application_info'])
         config = self.reader.get_config()
         self.assertEqual('SELF', config['product_abbreviation'])
+
+
+
+class TestKneReaderMMSKassenbuchAccountLabels(SampleDataReaderCase):
+    def setUp(self):
+        datafile = 'mms_kassenbuch_account_labels'
+        super(TestKneReaderMMSKassenbuchAccountLabels, self).setUp(datafile)
+    
+    def test_account_labels(self):
+        self.assertEqual(1, self.reader.get_number_of_files())
+        dfile = self.reader.get_file(0)
+        data_lines = dfile.get_master_data_lines()
+        self.assertEqual(19, len(data_lines))
+        line15 = data_lines[14]
+        self.assertEqual(4190, line15.key)
+        self.assertEqual(u"Aushilfslöhne", line15.text)
 
 
 
