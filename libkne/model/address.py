ae8838267b13f1e8055834c353921a13f3aa5078
Felix Schwarz <felix.schwarz@schwarz.eu>
2008-10-09 12:14:14 +0200

    split long values for master data

--- libkne/model/address.py
+++ libkne/model/address.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import logging
+import re
 
 from libkne import DataLine
 
@@ -12,24 +13,26 @@ __all__ = ["KNEAddress"]
 
 attr_codes = \
     dict(
-         new_record=101,
-         account_number = 102,
-         name1=103,
-         customer_number=104,
-         zip_code=106,
-         city=107,
-         street=108,
-         appellation=109,
-         name2=203,
-         additional_delivery_information=701,
-         nationality_key=702,
-         language=703,
-         phonenumber=710,
-         faxnumber=711,
-         emailaddress=712, #713
-         degree=801,
-         cellphonenumber=802,
-         interneturl=803, #804
+         new_record        = 101,
+         account_number    = 102,
+         name1             = 103,
+         customer_number   = 104,
+         zip_code          = 106,
+         city              = 107,
+         street            = 108,
+         appellation       = 109,
+         name2             = 203,
+         additional_delivery_information = 701,
+         nationality_key   = 702,
+         language          = 703,
+         phonenumber       = 710,
+         faxnumber         = 711,
+         emailaddress1     = 712,
+         emailaddress2     = 713,
+         degree            = 801,
+         cellphonenumber   = 802,
+         interneturl1      = 803,
+         interneturl2      = 804,
         )
 
 
@@ -40,6 +43,27 @@ class KNEAddress(object):
             first_kw = kwargs.keys()[0]
             raise ValueError("Unknown keyword parameter '%s'" % first_kw)
     
+    
+    def _build_list_of_splittable_attribut_names(self):
+        splittable_attribut_names = []
+        for key in attr_codes:
+            if key.endswith('1'):
+                base_name = key[:-1]
+                splittable_attribut_names.append(base_name)
+        return splittable_attribut_names
+    
+    
+    def _build_sorted_attrname_dict(self):
+        code_name_tuples = []
+        for name in attr_codes:
+            code = attr_codes[name]
+            code_name_tuples.append((code, name))
+        code_name_tuples.sort()
+        
+        attrnames = []
+        for code, name in code_name_tuples:
+            attrnames.append(name)
+        return attrnames
     
     def __init__(self, new_record, account_number, **kwargs):
         assert new_record in [True, False], 'new_record must in [True, False], not %s' % new_record
@@ -52,7 +76,63 @@ class KNEAddress(object):
             value = kwargs.pop(name, None)
             if not hasattr(self, name):
                 setattr(self, name, value)
+        self._splittable_attribut_names = self._build_list_of_splittable_attribut_names()
+        self._attrnames_sorted_by_key = self._build_sorted_attrname_dict()
         self._assert_no_unused_kw_arguments(kwargs)
+    
+    
+    def _split_long_values(self, attrname, value):
+        values = []
+        
+        max_data_length_single_line = 40
+        if not isinstance(value, basestring):
+            value = unicode(value)
+            if len(value) > max_data_length_single_line:
+                if attrname not in self._splittable_attribut_names:
+                    value = value[:max_data_length_single_line]
+                    base_msg = 'Line for attribute "%s" too long, cutting string: "%s..."'
+                    msg = base_msg % (attrname, value)
+                    log.warn(msg)
+                else:
+                    pass
+            else:
+                key = attr_codes[attrname]
+                values = [(key, value)]
+        return values
+    
+    
+    def _put_values_for_basenames_into_normal_attributes(self):
+        for base_name in self._splittable_attribut_names:
+            
+            value = getattr(self, base_name, None)
+            if value not in [None, '']:
+                number_of_items = 0
+                for key in attr_codes:
+                    regex = re.compile('^%s(\d+)$' % re.escape(base_name))
+                    match = regex.search(key)
+                    if match != None:
+                        number_of_items += 1
+                assert number_of_items > 0
+                
+                if not isinstance(value, basestring):
+                    value = unicode(value)
+                for i in range(number_of_items):
+                    
+                    start_index = i * 40
+                    if start_index > len(value):
+                        break
+                    end_index = start_index + 40
+                    part_value = value[start_index:end_index]
+                    
+                    part_key = base_name + str(i + 1)
+                    existing_value = getattr(self, part_key, None)
+                    if (existing_value != None) and (existing_value != part_value):
+                        base_msg = 'Tried to write %s "%s" into %s but this key already has value %s'
+                        msg = base_msg % (base_name, part_value, part_key, existing_value)
+                        raise ValueError(msg)
+                    setattr(self, part_key, part_value)
+            
+            
     
     
     def build_masterdata_lines(self):
@@ -64,17 +144,23 @@ class KNEAddress(object):
             line = DataLine(key=attr_codes[key], text=value)
             lines.append(line)
         
-        # TODO: Length check!
-        for name in attr_codes:
+        self._put_values_for_basenames_into_normal_attributes()
+        
+        for name in self._attrnames_sorted_by_key:
             value = getattr(self, name)
             key = attr_codes[name]
+            
             if value not in [None, ''] and name not in special_keys:
+                #key_value_pairs = self._split_long_values(name, value)
+                
+                if not isinstance(value, basestring):
+                    value = unicode(value)
                 if len(value) > 40:
                     value = value[:40]
                     base_msg = 'Line for attribute "%s" too long, cutting string: "%s..."'
                     msg = base_msg % (name, value)
                     log.warn(msg)
-                    print msg
+                assert len(value) <= 40
                 line = DataLine(key=key, text=value)
                 lines.append(line)
         return lines
--- tests/test_kneaddress.py
+++ tests/test_kneaddress.py
@@ -6,10 +6,46 @@ from libkne.model import KNEAddress
 
 
 class TestKNEAddress(unittest.TestCase):
+    def _assert_line_contents(self, key_text_tuples, computed_data):
+        self.assertEqual(len(key_text_tuples), len(computed_data))
+        for x in zip(key_text_tuples, computed_data):
+            print 'x', repr(x)
+        for (key, text), data in zip(key_text_tuples, computed_data):
+            self.assertEqual(key, data.key)
+            self.assertEqual(text, data.text)
+    
+    
     def test_cut_long_strings(self):
         kneaddr = KNEAddress(True, 10001)
         kneaddr.degree = 'Dipl.-Inform. (Univ) / Foo Bar Master Manager (BA)'
         lines = kneaddr.build_masterdata_lines()
         self.assertEqual(3, len(lines))
+    
+    
+    def test_transform_int_into_unicode_string(self):
+        kneaddr = KNEAddress(True, 10001)
+        kneaddr.appellation = 5
+        lines = kneaddr.build_masterdata_lines()
+        self.assertEqual(3, len(lines))
+    
+    
+    def test_distribute_long_values_acrosss_multiple_lines(self):
+        kneaddr = KNEAddress(True, 10001)
+        kneaddr.name = 'Foo Bar Baz Qux Quux Quuux Quuuux Quuuuux Barfoo'
+        lines = kneaddr.build_masterdata_lines()
+        self.assertEqual(4, len(lines))
+        expected = [(103, 'Foo Bar Baz Qux Quux Quuux Quuuux Quuuuu'), 
+                    (203, 'x Barfoo')]
+        self._assert_line_contents(expected, lines[2:])
+    
+    
+    def test_cut_splitted_values_if_too_long(self):
+        kneaddr = KNEAddress(True, 10001)
+        kneaddr.name = 'y'*40 + 'x'*30 + 'z'*30
+        lines = kneaddr.build_masterdata_lines()
+        self.assertEqual(4, len(lines))
+        self._assert_line_contents([(103, 'y'*40), (203, 'x'*30 + 'z'*10)], 
+                                   lines[2:])
+
 
 
