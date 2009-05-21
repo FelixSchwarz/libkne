d8d594aa12591988bff889f44e338122fc39f499
Felix Schwarz <felix.schwarz@schwarz.eu>
2009-05-21 11:56:49 +0200

    added setup.py to produce egg files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name          = 'libkne',
+      version       = '0.2.3',
+      license       = 'Proprietary - all rights reserved',
+      description   = 'Python library to read/write KNE files',
+      author        = 'Felix Schwarz',
+      author_email  = 'felix.schwarz@schwarz.eu',
+      url           = 'http://www.schwarz.eu',
+      packages      = ['libkne'],
+)
+
