9de4c67d85e50fda564805cad006ebb0283cf60c
Felix Schwarz <felix.schwarz@schwarz.eu>
2009-05-21 12:40:48 +0200

    switching back to development mode (next version will be 0.2.4)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
       name          = 'libkne',
-      version       = '0.2.3',
+      version       = '0.2.4',
       license       = 'Proprietary - all rights reserved',
       description   = 'Python library to read/write KNE files',
       author        = 'Felix Schwarz',
