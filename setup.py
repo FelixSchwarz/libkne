35cc3bd93ab2101fc9527dc27f6fb06c51a96466
Felix Schwarz <felix.schwarz@schwarz.eu>
2014-11-10 13:34:55 +0100

    fix unusual indentation in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-      name          = 'libkne',
-      version       = '0.2.4',
-      license       = 'Proprietary - all rights reserved',
-      description   = 'Python library to read/write KNE files',
-      author        = 'Felix Schwarz',
-      author_email  = 'felix.schwarz@schwarz.eu',
-      url           = 'http://www.schwarz.eu',
-      packages      = ['libkne'],
+    name          = 'libkne',
+    version       = '0.2.4',
+    license       = 'Proprietary - all rights reserved',
+    description   = 'Python library to read/write KNE files',
+    author        = 'Felix Schwarz',
+    author_email  = 'felix.schwarz@schwarz.eu',
+    url           = 'http://www.schwarz.eu',
+    packages      = ['libkne'],
 )
 
